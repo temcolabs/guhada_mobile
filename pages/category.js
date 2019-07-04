bb02f52c3cbf44977ec0689a3affb2af9dd3c2f3
kyungchul kim <kkc@temco.io>
2019-07-04 15:24:25 +0900

    - 카테고리 컴포넌트 및 로직 구현 완료

---
+++ pages/category.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
+import css from './category.module.scss';
+import { LinkRoute } from 'lib/router';
+
+@inject('category', 'searchitem')
+@observer
+class category extends Component {
+  grow(params) {
+    var growDiv = document.getElementById(params);
+    if (growDiv.clientHeight) {
+      growDiv.style.height = 0;
+    } else {
+      var wrapper = document.querySelector('.measuringWrapper');
+      growDiv.style.height = wrapper.clientHeight + 'px';
+    }
+  }
+
+  toSearch = category => {
+    let { searchitem } = this.props;
+    searchitem.toSearch(
+      category,
+      '',
+      1,
+      searchitem.unitPerPage,
+      '',
+      '',
+      '',
+      'category',
+      ''
+    );
+  };
+
+  render() {
+    const { category } = this.props;
+    let categoryList = category.category;
+
+    return (
+      <ul className={css.tree}>
+        {categoryList.map(categoryMain => {
+          return (
+            <li key={categoryMain.id}>
+              <input
+                type="checkbox"
+                id={`${categoryMain.title}${categoryMain.key}category`}
+                defaultChecked={false}
+              ></input>
+              <label
+                htmlFor={`${categoryMain.title}${categoryMain.key}category`}
+                onClick={() => this.grow(categoryMain.title + categoryMain.key)}
+              >
+                {categoryMain.title}
+              </label>
+              <ul id={`${categoryMain.title}${categoryMain.key}`}>
+                <div className="measuringWrapper">
+                  <li onClick={() => this.toSearch(categoryMain.id)}>
+                    전체보기
+                  </li>
+                  {categoryMain.children.map(categoryItem => {
+                    return (
+                      <LinkRoute
+                        href={`/categorydepth?category=${categoryItem.id}`}
+                        key={categoryItem.id}
+                      >
+                        <a>
+                          <li className={css.arrow}>{categoryItem.title}</li>
+                        </a>
+                      </LinkRoute>
+                    );
+                  })}
+                </div>
+              </ul>
+            </li>
+          );
+        })}
+        <LinkRoute href={`/brand`}>
+          <a>
+            <label>브랜드</label>
+          </a>
+        </LinkRoute>
+      </ul>
+    );
+  }
+}
+
+export default category;
---
+++ pages/categoryDepth.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
+import css from './categoryDepth.module.scss';
+import Router from 'next/router';
+
+@inject('category', 'searchitem')
+@observer
+class categoryDepth extends Component {
+  componentDidMount() {
+    const { category } = this.props;
+    let query = Router.router.query;
+
+    category.returnCategory(query.category);
+  }
+
+  grow(params) {
+    var growDiv = document.getElementById(params);
+    if (growDiv.clientHeight) {
+      growDiv.style.height = 0;
+    } else {
+      var wrapper = document.querySelector('.measuringWrapper');
+      growDiv.style.height = wrapper.clientHeight + 'px';
+    }
+  }
+
+  toSearch = category => {
+    let { searchitem } = this.props;
+    searchitem.toSearch(
+      category,
+      '',
+      1,
+      searchitem.unitPerPage,
+      '',
+      '',
+      '',
+      'category',
+      ''
+    );
+  };
+
+  render() {
+    const { category, searchitem } = this.props;
+
+    let categoryList = category.categoryList;
+
+    return (
+      <ul className={css.tree}>
+        <label onClick={() => this.toSearch(Router.router.query.category)}>
+          전체보기
+        </label>
+        {categoryList !== undefined && categoryList.children
+          ? categoryList.children.map(categoryMain => {
+              return (
+                <li key={categoryMain.id}>
+                  <input
+                    type="checkbox"
+                    id={`${categoryMain.title}${categoryMain.key}category`}
+                    defaultChecked={false}
+                  ></input>
+                  <label
+                    className={css.plus}
+                    htmlFor={`${categoryMain.title}${categoryMain.key}category`}
+                    onClick={() =>
+                      categoryMain.children
+                        ? this.grow(categoryMain.title + categoryMain.key)
+                        : null
+                    }
+                  >
+                    {categoryMain.title}
+                  </label>
+                  {categoryMain.children ? (
+                    <ul id={`${categoryMain.title}${categoryMain.key}`}>
+                      <div className="measuringWrapper">
+                        <li onClick={() => this.toSearch(categoryMain.id)}>
+                          전체보기
+                        </li>
+                        {categoryMain.children.map(categoryItem => {
+                          return (
+                            <li
+                              key={categoryItem.id}
+                              className={css.arrow}
+                              onClick={() => this.toSearch(categoryItem.id)}
+                            >
+                              {categoryItem.title}
+                            </li>
+                          );
+                        })}
+                      </div>
+                    </ul>
+                  ) : null}
+                </li>
+              );
+            })
+          : null}
+      </ul>
+    );
+  }
+}
+
+export default categoryDepth;
