4093b0e1d360fae2c5ff403770cd62253ae7785f
kyungchul kim <kkc@temco.io>
2019-07-05 14:23:50 +0900

    - 현재 Toolbar fix

--- components/toolbar/ToolBar.js
+++ components/toolbar/ToolBar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import css from './ToolBar.module.scss';
 import cn from 'classnames';
+import { LinkRoute } from 'lib/router';
 export default function ToolBar() {
   return (
     <div className={css.wrap}>
-      <div className={cn(css.itemWrap, css.category, css.selected)}>
-        카테고리
-      </div>
-      <div className={cn(css.itemWrap, css.brand)}>브랜드</div>
+      <LinkRoute href={`/category`}>
+        <div className={cn(css.itemWrap, css.category, css.selected)}>
+          카테고리
+        </div>
+      </LinkRoute>
+      <LinkRoute href={`/brand`}>
+        <div className={cn(css.itemWrap, css.brand)}>브랜드</div>
+      </LinkRoute>
       <div className={cn(css.itemWrap, css.home)}>홈</div>
       <div className={cn(css.itemWrap, css.community)}>커뮤니티</div>
       <div className={cn(css.itemWrap, css.mypage)}>마이페이지</div>
