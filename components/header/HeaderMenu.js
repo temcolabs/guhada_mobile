26a8e773a8e25b56b081d9a697c4985d7ed53f4e
kyungchul kim <kkc@temco.io>
2019-07-09 14:07:42 +0900

    - 햄버거 메뉴 로그인 라우팅

--- components/header/HeaderMenu.js
+++ components/header/HeaderMenu.js
@@ -3,6 +3,7 @@ import SlideIn, { slideDirection } from 'components/common/panel/SlideIn';
 import css from './HeaderMenu.module.scss';
 import Category from './item/Category';
 import { LinkRoute } from 'lib/router';
+import Router from 'next/router';
 /**
  * 헤더의 햄버거 버튼 클릭시 표시되는 메뉴
  */
@@ -17,9 +18,11 @@ export default function HeaderMenu({
     <SlideIn isVisible={isVisible} direction={slideDirection.LEFT}>
       <div className={css.wrapper}>
         <div className={css.topWrap}>
-          <LinkRoute href={`/login`}>
-            <a className={css.login}>로그인 해주세요</a>
-          </LinkRoute>
+          {/* <LinkRoute href={`/login`}> */}
+          <a className={css.login} onClick={() => Router.push('/login')}>
+            로그인 해주세요
+          </a>
+          {/* </LinkRoute> */}
           <div className={css.itemWrap}>
             <LinkRoute href={`/`}>
               <a>
