9fe35fdeb5e169b3cf976281fdd93de1c28e8829
kimkwan <kwan.kim@temco.io>
2019-08-30 17:07:42 +0900

    오타 수정

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app
       return handle(req, res);
     });
 
-    server.listen(PORT, '127,0.0.1', err => {
+    server.listen(PORT, '127.0.0.1', err => {
       if (err) throw err;
       console.log(`> Ready on mobile http://localhost:${PORT}`);
     });
