9ea69bfb3cb5a2e0507806ebcb857f0f9dba53f6
kimkwan <kwan.kim@temco.io>
2019-08-30 16:39:07 +0900

    script 파일 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -30,5 +30,20 @@ module.exports = {
         NODE_ENV: 'production',
       },
     },
+    {
+      name: 'guhada-mobile-stg',
+      script: 'server.js',
+      args: '',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
   ],
 };
