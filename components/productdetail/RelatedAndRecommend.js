f7a1878b86fdf8d60d83ac9bcaec3b8537f6db05
kyungchul kim <kkc@temco.io>
2019-09-04 12:54:28 +0900

    - 연관상품 추천상품 존재할 때만 랜더하도록 로직 추가

--- components/productdetail/RelatedAndRecommend.js
+++ components/productdetail/RelatedAndRecommend.js
@@ -64,54 +64,62 @@ export default function RelatedAndRecommend({
 }) {
   return (
     <div className={css.wrap}>
-      <div className={css.header}>판매자의 연관상품</div>
-      <div className={css.slideWrap}>
-        {dealsOfSameBrand.map(deal => {
-          return (
-            <div className={css.itemWrap} key={deal.dealId}>
-              <img
-                className={css.image}
-                src={deal.productImage.url}
-                alt={deal.productImage.name}
-              />
-              <div className={css.contentsWrap}>
-                <div className={css.brandWrap}>
-                  <div className={css.brand}>{deal.brandName}</div>
-                  <div className={css.season}>{deal.productSeason}</div>
+      {dealsOfSameBrand.length > 0 && (
+        <>
+          <div className={css.header}>판매자의 연관상품</div>
+          <div className={css.slideWrap}>
+            {dealsOfSameBrand.map(deal => {
+              return (
+                <div className={css.itemWrap} key={deal.dealId}>
+                  <img
+                    className={css.image}
+                    src={deal.productImage.url}
+                    alt={deal.productImage.name}
+                  />
+                  <div className={css.contentsWrap}>
+                    <div className={css.brandWrap}>
+                      <div className={css.brand}>{deal.brandName}</div>
+                      <div className={css.season}>{deal.productSeason}</div>
+                    </div>
+                    <div className={css.title}>{deal.dealName}</div>
+                    <div className={css.price}>
+                      {deal.sellPrice.toLocaleString()}
+                    </div>
+                  </div>
                 </div>
-                <div className={css.title}>{deal.dealName}</div>
-                <div className={css.price}>
-                  {deal.sellPrice.toLocaleString()}
+              );
+            })}
+          </div>
+        </>
+      )}
+      {dealsOfRecommend.length > 0 && (
+        <>
+          <div className={cn(css.header)}>추천상품</div>
+          <div className={css.slideWrap}>
+            {dealsOfRecommend.map(deal => {
+              return (
+                <div className={css.itemWrap} key={deal.dealId}>
+                  <img
+                    className={css.image}
+                    src={deal.productImage.url}
+                    alt={deal.productImage.name}
+                  />
+                  <div className={css.contentsWrap}>
+                    <div className={css.brandWrap}>
+                      <div className={css.brand}>{deal.brandName}</div>
+                      <div className={css.season}>{deal.productSeason}</div>
+                    </div>
+                    <div className={css.title}>{deal.dealName}</div>
+                    <div className={css.price}>
+                      {deal.sellPrice.toLocaleString()}
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className={cn(css.header)}>추천상품</div>
-      <div className={css.slideWrap}>
-        {dealsOfRecommend.map(deal => {
-          return (
-            <div className={css.itemWrap} key={deal.dealId}>
-              <img
-                className={css.image}
-                src={deal.productImage.url}
-                alt={deal.productImage.name}
-              />
-              <div className={css.contentsWrap}>
-                <div className={css.brandWrap}>
-                  <div className={css.brand}>{deal.brandName}</div>
-                  <div className={css.season}>{deal.productSeason}</div>
-                </div>
-                <div className={css.title}>{deal.dealName}</div>
-                <div className={css.price}>
-                  {deal.sellPrice.toLocaleString()}
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+              );
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 }
