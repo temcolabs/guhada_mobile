5491dfe609c19051c17d0e77c9c90be26267167b
kyungchul kim <kkc@temco.io>
2019-09-11 16:02:38 +0900

    - next/head viewport 적용 테스트

--- pages/login/login.js
+++ pages/login/login.js
@@ -21,9 +21,13 @@ class index extends React.Component {
             type="image/x-icon"
             href="/static/guhada.ico"
           />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+          />
         </Head>
         <div>
-          <Login form={Form.signIn}></Login>
+          <Login form={Form.signIn} />
         </div>
       </>
     );
