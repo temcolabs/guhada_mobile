c84cd87f7f93c97146a3dececc85da3588cf685c
kimkwan <kwan.kim@temco.io>
2019-09-17 15:21:06 +0900

    prod 설정 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -45,5 +45,20 @@ module.exports = {
         NODE_ENV: 'production',
       },
     },
+    {
+      name: 'guhada-mobile-prod',
+      script: 'server.js',
+      args: '',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
   ],
 };
