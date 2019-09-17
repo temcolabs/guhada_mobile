b7185a0aeab3110bb65d8f2f1ae422c502793f3c
kimkwan <kwan.kim@temco.io>
2019-09-17 14:39:58 +0900

    prod 서버 배포 스크립트 작성

---
+++ scripts/deploy-prod.sh
@@ -0,0 +1,17 @@
+# 배포 스크립트
+APP_NAME=guhada-mobile-prod
+HOST=mobile
+USER=ec2-user
+REPOSITORY=git@github.com:temcolabs/guhada_mobile.git
+BRANCH=master
+DEST_REPO=/home/guhada/web/guhada_mobile
+DEST_APP=/home/ec2-user/guhada_mobile
+
+echo "[git pull && build app]"
+cd $DEST_REPO && git checkout $BRANCH && git clean --f && git reset --hard && git pull && npm install && npm run build
+
+echo "\n[upload files with rsync]"
+rsync -arv -progress --delete -e "ssh -i ~/pem/guhada_prod.pem" --exclude-from './.rsyncignore' ./ $USER@$HOST:$DEST_APP
+
+echo "\n[install npm modules and restart pm2 instance at server side]"
+ssh -i ~/pem/guhada_prod.pem $USER@$HOST "cd $DEST_APP && npm install && pm2 reload ecosystem.config.js --only $APP_NAME --env production"
