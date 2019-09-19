cb2d50cf2c1e528f32c13cbed61b8ccb33ff671e
kyungchul kim <kkc@temco.io>
2019-09-19 20:03:09 +0900

    - TECH-2483 문의하기 모달 버그 수정 및 로직 추가

--- components/productdetail/ProductInquiry/ProductInquiry.js
+++ components/productdetail/ProductInquiry/ProductInquiry.js
@@ -6,8 +6,9 @@ import { inject, observer } from 'mobx-react';
 import { toJS } from 'mobx';
 import NewInquiry from './NewInquiry';
 import _ from 'lodash';
+import { loginStatus } from 'constant/';
 
-@inject('productdetail', 'login')
+@inject('productdetail', 'login', 'alert')
 @observer
 class ProductInquiry extends Component {
   state = {
@@ -24,7 +25,7 @@ class ProductInquiry extends Component {
   };
 
   render() {
-    const { productdetail, login, tabRefMap } = this.props;
+    const { productdetail, login, tabRefMap, alert } = this.props;
     const { deals, inquiryList } = productdetail;
     console.log('_.isNil(inquiryList)', _.isNil(inquiryList));
     return (
@@ -56,7 +57,11 @@ class ProductInquiry extends Component {
           <div>
             <button
               className={css.isColored}
-              onClick={() => this.setIsNewInquiryVisible(true)}
+              onClick={() =>
+                login.loginStatus === loginStatus.LOGIN_DONE
+                  ? this.setIsNewInquiryVisible(true)
+                  : alert.showAlert('로그인이 필요한 서비스입니다.')
+              }
             >
               상품 문의하기
             </button>
