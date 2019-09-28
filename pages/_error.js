8cf0be1ae9e485ff15dcbf99a267af7a76a44096
Seoyoung Jeong <seoyoung.jeong@temco.io>
2019-09-28 15:38:51 +0900

    데스크탑에서 모바일 웹에 접근하면 모바일 웹사이트로 리다이렉트 시키는 로직 추가

--- pages/_error.js
+++ pages/_error.js
@@ -13,8 +13,9 @@ class ErrorPage extends React.Component {
   componentDidMount() {
     if (typeof window === 'object') {
       const query = qs.parse(window.location.search.substring(1));
+
       /**
-       * 데스크탑에서 모바일로 돌렸다는 시그널이 있을 때.
+       * 데스크탑에서 모바일로 돌렸다는 시그널이 있을 알림 표시.
        * 구하다 웹 데스크탑 프로젝트의 _document.js 파일 참조.
        */
       if (query.fromDesktop) {
