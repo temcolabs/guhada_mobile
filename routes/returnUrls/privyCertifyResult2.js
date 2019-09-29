3ec6e2f7f8d8e62a8780efcea3cc5395ff193ae0
kimkwan <kwan.kim@temco.io>
2019-09-29 23:05:42 +0900

    결제 test

--- routes/returnUrls/privyCertifyResult2.js
+++ routes/returnUrls/privyCertifyResult2.js
@@ -12,5 +12,66 @@ module.exports = {
       authData,
       'authData.P_OID , authData , /privyCertifyResult2'
     );
+
+    if (authData.P_STATUS !== '00') {
+      console.log(authData, 'authData', authData.P_RMESG1, 'message');
+
+      if (req.query.cartList && authData.P_RMESG1) {
+        res.redirect(
+          '/orderpayment?cartList=' +
+            req.query.cartList +
+            '&resultMsg=' +
+            authData.P_RMESG1
+        );
+      } else {
+        res.redirect('/orderpayment?cartList=' + req.query.oid);
+      }
+
+      return false;
+    }
+
+    API.order
+      .post(
+        `/order/orderApproval`,
+        {
+          resultCode: authData.P_STATUS,
+          resultMsg: authData.P_RMESG1,
+          // pgMid: authData.P_TID,
+          // authToken: authData.authToken,
+          // authUrl: authData.authUrl,
+          // netCancel: authData.netCancelUrl,
+          checkAckUrl: authData.P_REQ_URL,
+          pgOid: req.query.oid,
+          pgTidSample: authData.P_TID,
+          web: false,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+      .then(response => {
+        let data = response.data.data;
+        console.log(response, 'response');
+        res.redirect('/orderpaymentsuccess?id=' + data);
+      })
+      .catch(err => {
+        if (err.status === 200) {
+          console.error(`err message ${err}`);
+          console.error(`pg 통과후 err ${err.data}`);
+          if (req.query.cartList) {
+            res.redirect('/orderpayment?cartList=' + req.query.cartList);
+          } else {
+            res.redirect('/orderpayment?oid=' + req.query.oid);
+          }
+        } else {
+          console.error(err);
+          res.redirect('/orderpayment?oid=' + req.query.oid);
+          // res.redirect(
+          //   `/orderpayment?cartList=${req.query.cartList}&resultMsg=${err}`
+          // );
+        }
+      });
   },
 };
