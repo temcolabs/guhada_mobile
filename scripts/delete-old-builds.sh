f238fb745e396059325b1aae3dc67a21fc8b530d
Seoyoung Jeong <seoyoung.jeong@temco.io>
2019-10-15 12:29:01 +0900

    모바일 배포 과정에서 오래된 빌드 파일 제거하는 과정 추가

--- scripts/delete-old-builds.sh
+++ scripts/delete-old-builds.sh
@@ -1,3 +1,5 @@
-# .next(빌드) 폴더에서 마지막으로 수정한 지 7일 이상인 파일들은 모두 삭제한다.
+# .next(빌드) 폴더에서 마지막으로 수정한 지 60분 이상인 파일을 모두 삭제한다.
+# 디렉토리로 지정하면 css chunks runtime 폴더가 삭제되기 때문에 안됨
 # ! 반드시 빌드가 끝난 후 실행해야 한다 !
-find ./.next/* -mtime +6 | xargs rm -rf
+find ./.next/server/static/* -type f -mmin +60 | xargs rm -rf
+find ./.next/static/* -type f -mmin +60 | xargs rm -rf
