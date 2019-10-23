e959a0891f46101fbff858895fe534bc4be64ec6
Seoyoung Jeong <seoyoung.jeong@temco.io>
2019-10-23 17:46:53 +0900

    prod 배포 cluster 모드로 변경

--- server.js
+++ server.js
@@ -28,6 +28,18 @@ app
     server.use(compression());
     server.use(express.urlencoded({ extended: false }));
     server.use(express.json());
+    server.enable('trust proxy', 'loopback');
+
+    let isAppGoingToBeClosed = false; // PM@의 SIGINT 시그널을 받았는지 여부. 앱이 곧 종료될 것임을 의미한다.
+
+    server.use(function(req, res, next) {
+      // 프로세스 종료 예정이라면 리퀘스트를 처리하지 않는다
+      if (isAppGoingToBeClosed) {
+        res.set('Connection', 'close');
+      }
+
+      next();
+    });
 
     // return urls
     for (const route of returnUrls) {
@@ -47,9 +59,27 @@ app
       return handle(req, res);
     });
 
-    server.listen(PORT, '127.0.0.1', err => {
+    const listeningServer = server.listen(PORT, '127.0.0.1', err => {
       if (err) throw err;
       console.log(`> Ready on mobile https://localhost:${PORT}`);
+
+      // PM2에게 앱 구동이 완료되었음을 전달한다
+      if (process.send) {
+        process.send('ready');
+        console.log('sent ready signal to PM2 at', new Date());
+      }
+    });
+
+    process.on('SIGINT', function() {
+      console.log('received SIGNIT signal');
+
+      isAppGoingToBeClosed = true; // 앱이 종료될 것
+
+      // pm2 재시작 신호가 들어오면 서버를 종료시킨다.
+      listeningServer.close(function(err) {
+        console.log('server closed');
+        process.exit(err ? 1 : 0);
+      });
     });
   })
   .catch(ex => {
