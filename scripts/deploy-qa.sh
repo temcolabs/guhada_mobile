44aa845648f8dce7179d2c73196a0add35e4249e
Seoyoung Jeong <seoyoung.jeong@temco.io>
2019-10-31 15:42:49 +0900

    qa 배포 스크립트 수정

--- scripts/deploy-qa.sh
+++ scripts/deploy-qa.sh
@@ -3,8 +3,16 @@ APP_NAME=guhada-mobile-qa
 HOST=52.78.149.242
 USER=ec2-user
 REPOSITORY=git@github.com:temcolabs/guhada_mobile.git
-BRANCH=release
 DEST_REPO=/home/ec2-user/guhada_mobile
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+RELEASE_VERSION=$(echo "release/${PACKAGE_VERSION}" | tr -d ' ')
+
+echo $RELEASE_VERSION
 
 # 서버 접속 후 테스트 앱 시작
-ssh -i ~/.ssh/guhada.pem $USER@$HOST "cd $DEST_REPO && git checkout $BRANCH && git reset --hard origin/$BRANCH && git pull && npm install && npm run build && pm2 flush && pm2 reload ecosystem.config.js --only $APP_NAME --env production && ./scripts/delete-old-builds.sh"
+ssh -i ~/.ssh/guhada.pem $USER@$HOST "cd $DEST_REPO && git remote update && git checkout $RELEASE_VERSION && git reset --hard $RELEASE_VERSION && git pull && npm install && npm run build && pm2 flush && APP_NAME=$APP_NAME npm run reload-pm2 && ./scripts/delete-old-builds.sh"
