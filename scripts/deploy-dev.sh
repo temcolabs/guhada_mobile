daf6b71bf682264b73feb84f956f3cd815700a7f
Seoyoung Jeong <seoyoung.jeong@temco.io>
2019-11-01 16:20:48 +0900

    git remote update --prune 옵션 추가

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -7,5 +7,5 @@ BRANCH=develop
 DEST_REPO=/home/ec2-user/guhada_mobile
 
 # 서버 접속 후 테스트 앱 시작
-ssh -i ~/.ssh/guhada.pem $USER@$HOST "cd $DEST_REPO && git remote update && git checkout $BRANCH && git reset --hard $BRANCH && git pull && npm install && npm run build && pm2 flush && pm2 reload ecosystem.config.js --only $APP_NAME --env production && ./scripts/delete-old-builds.sh"
+ssh -i ~/.ssh/guhada.pem $USER@$HOST "cd $DEST_REPO && git remote update --prune && git checkout $BRANCH && git reset --hard $BRANCH && git pull && npm install && npm run build && pm2 flush && pm2 reload ecosystem.config.js --only $APP_NAME --env production && ./scripts/delete-old-builds.sh"
 
