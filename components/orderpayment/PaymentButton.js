aca3f3c8b608288b6676d251e4233ac87fe4b91f
kimkwan <kwan.kim@temco.io>
2019-11-08 15:02:01 +0900

    주문결제 - 이용약관 동의 확인 팝업추가

--- components/orderpayment/PaymentButton.js
+++ components/orderpayment/PaymentButton.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import css from './PaymentButton.module.scss';
-@inject('orderpayment')
+@inject('orderpayment', 'alert')
 @observer
 class PaymentButton extends Component {
   render() {
-    let { orderpayment } = this.props;
+    let { orderpayment, alert } = this.props;
 
     return orderpayment.status.orderPaymentAgreement ? (
       <div
@@ -18,7 +18,16 @@ class PaymentButton extends Component {
         결제하기
       </div>
     ) : (
-      <div className={css.wrap} style={{ backgroundColor: '#ddd' }}>
+      <div
+        className={css.wrap}
+        style={{ backgroundColor: '#ddd' }}
+        onClick={() => {
+          alert.showAlert({
+            content:
+              '구매 및 결제대행서비스 이용약관 확인 후 동의하셔야 구매가 가능합니다.',
+          });
+        }}
+      >
         결제하기
       </div>
     );
