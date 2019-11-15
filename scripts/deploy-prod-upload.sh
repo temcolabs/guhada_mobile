ff07a3eb7e585709a67933aa5e09e53d1fb7f80b
Seoyoung Jeong <seoyoung.jeong@temco.io>
2019-11-15 22:01:00 +0900

    rsync 에서 delete 옵션 제거

--- scripts/deploy-prod-upload.sh
+++ scripts/deploy-prod-upload.sh
@@ -7,7 +7,7 @@ DEST_REPO=/home/guhada/web/guhada_mobile
 DEST_APP=/home/ec2-user/guhada_mobile
 
 echo "\n[$HOST: upload files]"
-rsync -arv -progress --delete -e "ssh -i ~/pem/guhada_prod.pem" --exclude-from './.rsyncignore' ./ $USER@$HOST:$DEST_APP
+rsync -arv -progress -e "ssh -i ~/pem/guhada_prod.pem" --exclude-from './.rsyncignore' ./ $USER@$HOST:$DEST_APP
 
 echo "\n[$HOST: install npm modules and restart pm2]"
 ssh -i ~/pem/guhada_prod.pem $USER@$HOST "cd $DEST_APP && npm install && pm2 flush && pm2 reload ecosystem.config.js --only $APP_NAME --env production"
