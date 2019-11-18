6ce3aa4c8b909dddeb5a03305face08b302d8227
Seoyoung Jeong <seoyoung.jeong@temco.io>
2019-11-18 13:52:18 +0900

    mgmt 빌드 파일 제거 과정 추가

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -5,7 +5,6 @@ USER=ec2-user
 REPOSITORY=git@github.com:temcolabs/guhada_mobile.git
 BRANCH=master
 DEST_REPO=/home/guhada/web/guhada_mobile
-DEST_APP=/home/ec2-user/guhada_mobile
 
 echo "[git pull && build app]"
 cd $DEST_REPO && git remote update --prune && git checkout $BRANCH && git reset --hard $BRANCH && git pull && rm -rf .next && npm install && npm run build
@@ -14,3 +13,7 @@ HOST=$HOST ./scripts/deploy-prod-upload.sh
 
 # 로드 밸런싱을 사용한다면 동시 배포
 # HOST=$HOST1 ./scripts/deploy-prod-upload.sh & HOST=$HOST2 ./scripts/deploy-prod-upload.sh
+
+echo "> delete old build files from build server"
+exit
+sh $DEST_REPO/scripts/delete-old-builds.sh
