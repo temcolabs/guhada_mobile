0fd6c19841e43c44f7dfef6870886bd7ead0d88b
Seoyoung Jeong <seoyoung.jeong@temco.io>
2019-11-20 17:56:23 +0900

    lib 폴더에서 API, route, 네이버 트래커 제외하고 childs/lib 사용

---
+++ common/autoTelNumber.js
@@ -0,0 +1,17 @@
+import { autoHypenTele, autoHypenPhone } from 'utils';
+/**
+ * field.onChange에서 사용할 전화번호에 - 넣는 라이브러리
+ *
+ * api 콜을 하기위해서는
+ * form.values().mobile.replace(/-/gi, ''),
+ * 위와 같은 방식으로 - 제거가 필요하다.
+ */
+export default function autoTelNumber(field) {
+  if (field.get('type') === 'tel') {
+    if (field.path === 'managerTelephone' || field.path === 'fax') {
+      field.set(autoHypenTele(field.value));
+    } else {
+      field.set(autoHypenPhone(field.value));
+    }
+  }
+}
