2a39c39add4f31e25c5d0f14a84e7b1e6cd0a6e1
kyungchul kim <kkc@temco.io>
2019-11-21 14:33:01 +0900

    - banner const mobile attr, gunuine 배너 추가

--- constant/banner.js
+++ constant/banner.js
@@ -6,6 +6,9 @@ export const bannerInfo = [
     mg: `${
       process.env.API_CLOUD
     }/images/banner/luckydraw/lucky_main_pc_1280.png`,
+    mobile: `${
+      process.env.API_CLOUD
+    }/images/banner/luckydraw/lucky_main_m_360.png`,
     com: `${process.env.API_CLOUD}/images/banner/luckydraw/lucky_com_pc.png`,
     href: `/event/luckydraw`,
     bgColor: '#121933',
@@ -17,6 +20,9 @@ export const bannerInfo = [
     mg: `${
       process.env.API_CLOUD
     }/images/banner/timedeal/timedeal_main_pc1280.png`,
+    mobile: `${
+      process.env.API_CLOUD
+    }/images/banner/timedeal/timedeal_main_m_360.png`,
     com: `${process.env.API_CLOUD}/images/banner/timedeal/timedeal_com_pc.png`,
     href: `/event/timedeal`,
     bgColor: '#7d32d1',
@@ -28,6 +34,9 @@ export const bannerInfo = [
     mg: `${
       process.env.API_CLOUD
     }/images/banner/signup5000/join_main_pc_1280.png`,
+    mobile: `${
+      process.env.API_CLOUD
+    }/images/banner/signup5000/join_main_m_360.png`,
     com: `${process.env.API_CLOUD}/images/banner/signup5000/join_com_pc.png`,
     href: `/`,
     bgColor: '#8172f0',
@@ -39,9 +48,26 @@ export const bannerInfo = [
     mg: `${
       process.env.API_CLOUD
     }/images/banner/2perdiscount/2per_main_pc_1280.png`,
+    mobile: `${
+      process.env.API_CLOUD
+    }/images/banner/2perdiscount/2per_main_m_360.png`,
     com: ``,
     href: `/`,
     bgColor: '#540093',
+  },
+  {
+    lg: `${
+      process.env.API_CLOUD
+    }/images/banner/genuine/genuine_main_pc_1920.png`,
+    mg: `${
+      process.env.API_CLOUD
+    }/images/banner/genuine/genuine_main_pc_1280.png`,
+    mobile: `${
+      process.env.API_CLOUD
+    }/images/banner/genuine/genuine_main_m_360.png`,
+    com: ``,
+    href: `/`,
+    bgColor: '#021023',
   },
 ];
 export default bannerInfo;
