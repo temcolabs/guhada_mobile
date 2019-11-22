e75ec5b903d5f1b58b4ea4293aa65560f630fa8c
Seoyoung Jeong <seoyoung.jeong@temco.io>
2019-11-22 12:58:37 +0900

    상품상세 헤드 태그 수정

--- pages/product/productdetail.js
+++ pages/product/productdetail.js
@@ -3,16 +3,39 @@ import ProductDetail from '../../template/product/ProductDetail';
 import { inject, observer } from 'mobx-react';
 import { getParameterByName } from '../../utils';
 import Loading from '../../components/common/loading/Loading';
-import { withRouter } from 'next/router';
+import Router, { withRouter } from 'next/router';
 import withScrollToTopOnMount from 'components/common/hoc/withScrollToTopOnMount';
 import criteoTracker from 'childs/lib/tracking/criteo/criteoTracker';
 import HeadForSEO from 'childs/lib/components/HeadForSEO';
+import API from 'childs/lib/API';
+import _ from 'lodash';
+import isServer from 'childs/lib/common/isServer';
 
 @withScrollToTopOnMount
 @withRouter
 @inject('productdetail', 'productDetailLike', 'user')
 @observer
 class index extends React.Component {
+  static async getInitialProps({ req }) {
+    const dealsId = isServer ? req.query.deals : Router.query.deals;
+    const { data } = await API.product.get(`/deals/${dealsId}`);
+    const deals = data.data;
+
+    const headData = {
+      pageName: `${_.isNil(deals.season) === false ? deals.season : ''} ${
+        deals.name
+      }`,
+      description: `${deals.brandName} - ${deals.name} - ${
+        deals.discountPrice
+      }원`,
+      image: _.get(deals, 'imageUrls.0'),
+    };
+
+    return {
+      headData,
+    };
+  }
+
   componentDidMount() {
     let { productdetail, productDetailLike, user } = this.props;
     let dealId = getParameterByName('deals');
@@ -35,6 +58,7 @@ class index extends React.Component {
     if (prevProps.router.query.deals !== this.props.router.query.deals) {
       let dealsId = getParameterByName('deals');
       productdetail.getDeals(dealsId);
+      window.scrollTo(0, 0);
     }
   }
 
@@ -42,11 +66,15 @@ class index extends React.Component {
     // window.removeEventListener('scroll', this.props.productdetail.tabInfoFixed);
   }
   render() {
-    let { productdetail } = this.props;
+    let { productdetail, headData } = this.props;
 
     return (
       <>
-        <HeadForSEO pageName="상품-상세페이지" />
+        <HeadForSEO
+          pageName={headData?.pageName}
+          description={headData?.description}
+          image={headData?.image}
+        />
 
         <div>
           {productdetail.dealsStatus ? (
