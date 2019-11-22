c854b1e6644c1bb99bbfd8260516cf0b2e9b055c
Seoyoung Jeong <seoyoung.jeong@temco.io>
2019-11-22 12:38:34 +0900

    static.getInitialProps 유지

--- components/common/hoc/withScrollToTopOnMount.js
+++ components/common/hoc/withScrollToTopOnMount.js
@@ -1,17 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 /**
  * 마운트 완료시 스크롤을 최상단으로 옮긴다
  */
 export default function withScrollToTopOnMount(BaseComponent) {
-  return class wrappedComponent extends Component {
+  class wrappedComponent extends Component {
     componentDidMount(prevProps) {
-      window.scrollTo(0, 0)
+      window.scrollTo(0, 0);
     }
 
     render() {
-      const passedProps = Object.assign({}, this.props, {})
-      return <BaseComponent {...passedProps} />
+      const passedProps = Object.assign({}, this.props, {});
+      return <BaseComponent {...passedProps} />;
     }
   }
+
+  if (BaseComponent.getInitialProps) {
+    wrappedComponent.getInitialProps = BaseComponent.getInitialProps;
+  }
+
+  return wrappedComponent;
 }
