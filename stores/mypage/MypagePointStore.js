0dc234f32d10ad3d1fb1b74c8c94a5f37c35bd6c
kimkwan <kwan.kim@temco.io>
2019-11-25 10:21:34 +0900

    mypage 포인트 페이지 작업중

--- stores/mypage/MypagePointStore.js
+++ stores/mypage/MypagePointStore.js
@@ -1,11 +1,9 @@
-import { observable, action, toJS } from 'mobx';
+import { observable, action } from 'mobx';
 import API from 'childs/lib/API';
-const isServer = typeof window === 'undefined';
 import { isBrowser } from 'childs/lib/common/isServer';
-import { dateFormat } from 'constant/date';
+import { dateFormat } from 'childs/lib/constant/date';
 import moment from 'moment';
 import { devLog } from 'childs/lib/common/devLog';
-import { getParameterByName } from 'utils';
 
 export default class MypagePointStore {
   constructor(root) {
@@ -30,13 +28,18 @@ export default class MypagePointStore {
   @observable totalItemsCount = 0;
   @observable itemsCountPerPage = 10;
   @observable page = 1;
+
   @action
   getPointSummary = () => {
-    API.benefit.get(`/summary`).then(res => {
-      if (res.data.resultCode === 200) {
+    API.benefit
+      .get(`/summary`)
+      .then(res => {
         this.pointSummary = res.data.data;
-      }
-    });
+        devLog(this.pointSummary, 'this.pointSummary');
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   @action
@@ -48,44 +51,128 @@ export default class MypagePointStore {
     API.benefit
       .get(`/histories?fromAt=${startDate}&toAt=${endDate}&page=${pageNo}`)
       .then(res => {
-        if (res.data.resultCode === 200) {
-          this.period.startDate = startDate;
-          this.period.endDate = endDate;
-          this.pointHistory = res.data.data;
-          this.totalItemsCount = res.data.data.totalElements;
-          this.page = pageNo;
-          devLog(res.data, '포인트 히스토리');
-        }
+        this.period.startDate = startDate;
+        this.period.endDate = endDate;
+        this.pointHistory = res.data.data;
+        this.totalItemsCount = res.data.data.totalElements;
+        this.page = pageNo;
+        devLog(res.data, '포인트 히스토리');
+      })
+      .catch(err => {
+        console.log(err);
+        // this.root.alert.showAlert({
+        //   content: `${_.get(err, 'data.message') || 'ERROR'}`,
+        // });
       });
 
-    let paymentRemainCheck = JSON.parse(
-      sessionStorage.getItem('paymentInfoPoint')
-    );
+    // let paymentRemainCheck = JSON.parse(
+    //   sessionStorage.getItem('paymentInfoPoint')
+    // );
 
-    if (paymentRemainCheck) {
-      let resultMsg = getParameterByName('resultMsg');
+    // if (paymentRemainCheck) {
+    //   let resultMsg = getParameterByName('resultMsg');
 
-      if (resultMsg !== 'chargesuccess') {
-        this.root.alert.showAlert({
-          content: resultMsg || '포인트 충전 실패.',
-        });
-        this.pointChargeModal = true;
-      } else {
-        this.root.alert.showAlert({
-          content: '포인트 충전 성공',
-        });
-        sessionStorage.removeItem('paymentInfoPoint');
-      }
+    //   if (resultMsg !== 'chargesuccess') {
+    //     this.root.alert.showAlert({
+    //       content: resultMsg || '포인트 충전 실패.',
+    //     });
+    //     this.pointChargeModal = true;
+    //   } else {
+    //     this.root.alert.showAlert({
+    //       content: '포인트 충전 성공',
+    //     });
+    //     sessionStorage.removeItem('paymentInfoPoint');
+    //   }
+    // }
+  };
+
+  //   @action
+  //   pointChargeModalOpen = () => {
+  //     this.pointChargeModal = true;
+  //   };
+
+  //   @action
+  //   pointChargeModalClose = () => {
+  //     this.pointChargeModal = false;
+  //   };
+
+  @action
+  pointDelete = id => {
+    this.root.alert.showConfirm({
+      content: '포인트 내역을 삭제 하시겠습니까?',
+      onConfirm: () => {
+        API.benefit
+          .delete(`/histories/${id}`)
+          .then(res => {
+            if (res.status === 200) {
+              this.root.alert.showAlert({
+                content: '포인트가 삭제 되었습니다.',
+              });
+
+              for (let i = 0; i < this.pointHistory.content.length; i++) {
+                if (this.pointHistory.content[i].id === id) {
+                  this.pointHistory.content.splice(i, 1);
+                }
+              }
+
+              devLog(this.pointHistory.content, 'this.pointHistory');
+            }
+          })
+          .catch(err => {
+            console.log(err);
+            // this.root.alert.showAlert({
+            //   content: `${_.get(err, 'data.message') || 'ERROR'}`,
+            // });
+          });
+      },
+    });
+  };
+
+  // ============================================================
+  // 포인트 적립 알림 모달 관련 변수, 메소드
+  // ============================================================
+  @observable
+  isPointSavingModalOpen = false;
+
+  defaultSavedPointResponse = {
+    dueSavedPoint: 0,
+    message: '포인트 적립 완료!',
+    saveTargetType: 'BUY',
+    savedPoint: 0,
+    totalFreePoint: 0,
+    totalPaidPoint: 0,
+  };
+
+  @observable
+  savedPointResponse = Object.assign({}, this.defaultSavedPointResponse);
+
+  @action
+  setSavedPointResponse = (data = this.defaultSavedPointResponse) => {
+    this.savedPointResponse = data;
+  };
+  @action
+  resetSavedPointResponse = () => {
+    this.savedPointResponse = Object.assign({}, this.defaultSavedPointResponse);
+  };
+
+  @action
+  openPointSavingModal = (data = this.defaultSavedPointResponse) => {
+    if (data) {
+      this.setSavedPointResponse(data);
+      this.isPointSavingModalOpen = true;
+      this.root.mypageDashboard.getDashboard(); // 대시보드 새로고침
+    } else {
+      console.error('savedPointResponse 데이터가 없습니다', data);
     }
   };
 
   @action
-  pointChargeModalOpen = () => {
-    this.pointChargeModal = true;
-  };
+  closePointSavingModalOpen = () => {
+    this.isPointSavingModalOpen = false;
 
-  @action
-  pointChargeModalClose = () => {
-    this.pointChargeModal = false;
+    // modal close timeout
+    setTimeout(() => {
+      this.resetSavedPointResponse();
+    }, 400);
   };
 }
--- template/mypage/PointHistory.js
+++ template/mypage/PointHistory.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import { withRouter } from 'next/router';
 import DefaultLayout from 'components/layout/DefaultLayout';
-
+import css from './PointHistory.module.scss';
 import MyPageLayout from 'components/mypage/MyPageLayout';
+import PointDashboard from 'components/mypage/point/PointDashboard';
+import PeriodSelector from 'components/mypage/PeriodSelector';
+import { dateUnit } from 'childs/lib/constant/date';
+import { inject, observer } from 'mobx-react';
 
 @withRouter
+@inject('mypagePoint')
+@observer
 class PointHistory extends React.Component {
+  componentDidMount() {
+    this.handleChangePeriod();
+    this.getPointSummary();
+  }
+
+  getPointSummary = () => {
+    const { mypagePoint } = this.props;
+
+    mypagePoint.getPointSummary();
+  };
+
+  handleChangePeriod = ({ startDate, endDate } = {}) => {
+    const { mypagePoint } = this.props;
+
+    mypagePoint.getPointHistory({
+      startDate,
+      endDate,
+      pageNo: 1,
+    });
+  };
+
+  handleChangePage = page => {
+    this.props.mypagePoint.getPointHistory({ pageNo: page });
+  };
+
   render() {
+    let { mypagePoint } = this.props;
     return (
       <DefaultLayout
         topLayout={'main'}
@@ -15,7 +47,23 @@ class PointHistory extends React.Component {
         headerShape={'mypage'}
       >
         <MyPageLayout>
-          <div>포인트 히스토리</div>
+          <div className={css.wrap}>
+            <PointDashboard props={mypagePoint.pointSummary} />
+
+            <PeriodSelector
+              defaultTabItems={[
+                { value: 1, unit: dateUnit.WEEK },
+                { value: 15, unit: dateUnit.DAY },
+                { value: 30, unit: dateUnit.DAY },
+                { value: 3, unit: dateUnit.MONTH },
+                { value: 6, unit: dateUnit.MONTH },
+                { value: 1, unit: dateUnit.YEAR },
+                { value: 3, unit: dateUnit.YEAR },
+              ]}
+              monthlyTabRange={0}
+              onChangePeriod={this.handleChangePeriod}
+            />
+          </div>
         </MyPageLayout>
       </DefaultLayout>
     );
