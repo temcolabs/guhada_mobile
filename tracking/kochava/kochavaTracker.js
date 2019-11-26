d972d07aab7dfd61fc142b8861f50a1b27a8e7fa
kimkwan <kwan.kim@temco.io>
2019-11-26 17:21:56 +0900

    코차바 트래커 추가

---
+++ tracking/kochava/kochavaTracker.js
@@ -0,0 +1,36 @@
+export default {
+  shoppingCart: ({
+    dealId = null,
+    productId = null,
+    brandId = null,
+    sellerId = null,
+    season = null,
+    name = null,
+    sellPrice = null,
+    discountPrice = null,
+  }) => {
+    kochava.activity('Add_To_Cart', {
+      dealId,
+      productId,
+      brandId,
+      sellerId,
+      season,
+      name,
+      sellPrice,
+      discountPrice,
+    });
+  },
+  purchaseComplete: ({ successInfo }) => {
+    for (let i = 0; i < successInfo.orderList.length; i++) {
+      kochava.activity('Buy_Product', {
+        dealId: successInfo.orderList[i].dealId,
+        season: successInfo.orderList[i].season,
+        name: successInfo.orderList[i].prodName,
+        originalPrice: successInfo.orderList[i].originalPrice,
+        discountPrice: successInfo.orderList[i].discountPrice,
+        brandName: successInfo.orderList[i].brandName,
+        sellerId: successInfo.orderList[i].sellerId,
+      });
+    }
+  },
+};
