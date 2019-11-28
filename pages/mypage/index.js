93c24682e089519416a0b295b24bd1a2d3792531
Seoyoung Jeong <seoyoung.jeong@temco.io>
2019-11-28 17:02:37 +0900

    mypage 접근시 주문배송 페이지로 리다이렉트

---
+++ pages/mypage/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { pushRoute } from 'lib/router';
+
+/**
+ * /mypage 라우트
+ * 주문배송 목록으로 보냄
+ */
+class MypageIndex extends React.Component {
+  static async getInitialProps({ store }) {
+    return {};
+  }
+
+  componentDidMount() {
+    pushRoute(`/mypage/orders/complete/list`, { replace: true });
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+export default MypageIndex;
