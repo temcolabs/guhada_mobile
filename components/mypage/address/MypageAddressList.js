42ff262190fd52bdd0aac07c6fc7b33f804769b1
kimkwan <kwan.kim@temco.io>
2019-11-29 17:34:32 +0900

    마이페이지 - 배송지 진행중

--- components/mypage/address/MypageAddressList.js
+++ components/mypage/address/MypageAddressList.js
@@ -11,55 +11,55 @@ class MypageAddressList extends React.Component {
 
     return (
       <div className={css.wrap}>
-        {mypageAddress.addressList.length > 0
-          ? mypageAddress.addressList.map((data, index) => {
-              return (
-                <div className={css.addressItem} key={index}>
-                  <div className={css.section}>
-                    <div className={css.addressTitleWrap}>
-                      <div className={css.title}>
-                        {data.shippingName ? data.shippingName : ''}
-                      </div>
-                      {data.defaultAddress ? (
-                        <div className={css.defaultAddress} />
-                      ) : null}
+        {mypageAddress.addressList.length > 0 ? (
+          mypageAddress.addressList.map((data, index) => {
+            return (
+              <div className={css.addressItem} key={index}>
+                <div className={css.section}>
+                  <div className={css.addressTitleWrap}>
+                    <div className={css.title}>
+                      {data.shippingName ? data.shippingName : ''}
                     </div>
-
-                    <div className={css.utilButton}>
-                      <div
-                        onClick={() => {
-                          mypageAddress.editAddressModal(data.id);
-                        }}
-                      >
-                        수정
-                      </div>
-                      <div
-                        onClick={() => {
-                          mypageAddress.deleteAddress(data.id);
-                        }}
-                      >
-                        삭제
-                      </div>
-                    </div>
+                    {data.defaultAddress ? (
+                      <div className={css.defaultAddress} />
+                    ) : null}
                   </div>
 
-                  <div className={css.section}>
-                    <div className={css.address}>
-                      {`[${data.zip}] ${data.roadAddress} ${
-                        data.detailAddress
-                      }`}
+                  <div className={css.utilButton}>
+                    <div
+                      onClick={() => {
+                        mypageAddress.editAddressModal(data.id);
+                      }}
+                    >
+                      수정
                     </div>
-                  </div>
-
-                  <div className={css.section}>
-                    <div className={css.customer}>{`${
-                      data.recipientName
-                    } ${addHyphenToMobile(data.recipientMobile)}`}</div>
+                    <div
+                      onClick={() => {
+                        mypageAddress.deleteAddress(data.id);
+                      }}
+                    >
+                      삭제
+                    </div>
                   </div>
                 </div>
-              );
-            })
-          : 'no data'}
+
+                <div className={css.section}>
+                  <div className={css.address}>
+                    {`[${data.zip}] ${data.roadAddress} ${data.detailAddress}`}
+                  </div>
+                </div>
+
+                <div className={css.section}>
+                  <div className={css.customer}>{`${
+                    data.recipientName
+                  } ${addHyphenToMobile(data.recipientMobile)}`}</div>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <DataEmpty text="등록된 배송지가 없습니다." />
+        )}
       </div>
     );
   }
