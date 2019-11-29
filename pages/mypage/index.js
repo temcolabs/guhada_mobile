6178dbb120f04291785152131915b54770fc5029
Seoyoung Jeong <seoyoung.jeong@temco.io>
2019-11-29 12:04:38 +0900

    마이페이지 루트에 withAuth 데코레이터 추가

--- pages/mypage/index.js
+++ pages/mypage/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { pushRoute } from 'childs/lib/router';
+import withAuth from 'components/common/hoc/withAuth';
 
 /**
  * /mypage 라우트
  * 주문배송 목록으로 보냄
  */
+
+@withAuth({ isAuthRequired: true })
 class MypageIndex extends React.Component {
   static async getInitialProps({ store }) {
     return {};
