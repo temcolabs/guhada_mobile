42d82a1270a4c33fdb8b6caab92accf8c3160c41
kimkwan <kwan.kim@temco.io>
2019-11-29 17:31:31 +0900

    마이페이지 배송지 진행중

---
+++ components/mypage/address/MypageAddressList.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import css from './MypageAddressList.module.scss';
+import { inject, observer } from 'mobx-react';
+import addHyphenToMobile from 'childs/lib/string/addHyphenToMobile';
+import DataEmpty from 'components/common/DataEmpty';
+@inject('mypageAddress')
+@observer
+class MypageAddressList extends React.Component {
+  render() {
+    let { mypageAddress } = this.props;
+
+    return (
+      <div className={css.wrap}>
+        {mypageAddress.addressList.length > 0
+          ? mypageAddress.addressList.map((data, index) => {
+              return (
+                <div className={css.addressItem} key={index}>
+                  <div className={css.section}>
+                    <div className={css.addressTitleWrap}>
+                      <div className={css.title}>
+                        {data.shippingName ? data.shippingName : ''}
+                      </div>
+                      {data.defaultAddress ? (
+                        <div className={css.defaultAddress} />
+                      ) : null}
+                    </div>
+
+                    <div className={css.utilButton}>
+                      <div
+                        onClick={() => {
+                          mypageAddress.editAddressModal(data.id);
+                        }}
+                      >
+                        수정
+                      </div>
+                      <div
+                        onClick={() => {
+                          mypageAddress.deleteAddress(data.id);
+                        }}
+                      >
+                        삭제
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className={css.section}>
+                    <div className={css.address}>
+                      {`[${data.zip}] ${data.roadAddress} ${
+                        data.detailAddress
+                      }`}
+                    </div>
+                  </div>
+
+                  <div className={css.section}>
+                    <div className={css.customer}>{`${
+                      data.recipientName
+                    } ${addHyphenToMobile(data.recipientMobile)}`}</div>
+                  </div>
+                </div>
+              );
+            })
+          : 'no data'}
+      </div>
+    );
+  }
+}
+
+export default MypageAddressList;
--- template/mypage/AddressManagement.js
+++ template/mypage/AddressManagement.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { withRouter } from 'next/router';
+import css from './AddressManagement.module.scss';
 import DefaultLayout from 'components/layout/DefaultLayout';
-
 import MypageLayout from 'components/mypage/MypageLayout';
+import MypageAddressList from 'components/mypage/address/MypageAddressList';
+import MypageAddressModal from 'components/mypage/address/MypageAddressModal';
 import { inject, observer } from 'mobx-react';
 @inject('mypageAddress')
 @observer
 class AddressManagement extends Component {
+  componentDidMount() {
+    this.props.mypageAddress.getAddressList();
+  }
   render() {
+    let { mypageAddress } = this.props;
     return (
       <DefaultLayout
         topLayout={'main'}
@@ -16,7 +22,20 @@ class AddressManagement extends Component {
         headerShape={'mypage'}
       >
         <MypageLayout>
-          <div>배송지 관리</div>
+          <div className={css.wrap}>
+            <div
+              className={css.newAddress}
+              onClick={() => {
+                mypageAddress.newAddressModal();
+              }}
+            >
+              신규배송지 등록
+            </div>
+
+            <MypageAddressList />
+
+            <MypageAddressModal isOpen={mypageAddress.isModalOpen} />
+          </div>
         </MypageLayout>
       </DefaultLayout>
     );
