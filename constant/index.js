f4bbfd4fbb87e2e2453c6bad056230114e24e1a3
Seoyoung Jeong <seoyoung.jeong@temco.io>
2019-11-29 12:52:50 +0900

    constant 참조 추가수정

--- constant/index.js
+++ constant/index.js
@@ -1,4 +1,3 @@
 export { dateUnit, dateFormat } from './date';
 export { default as key } from './key';
 export { default as loginStatus } from './loginStatus';
-export { default as purchaseStatus } from './purchaseStatus';
