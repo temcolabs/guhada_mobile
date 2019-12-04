5b1fbc807d87b15beec0f1445babf1d20e3dd7e4
kyungchul kim <kkc@temco.io>
2019-12-04 11:17:19 +0900

    MypageData 빈 경우 보여주는 컴포넌트 추가

---
+++ components/mypage/MypageDataEmpty.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import css from './MypageDataEmpty.module.scss';
+
+function MypageDataEmpty({ text }) {
+  return (
+    <div className={css.wrap}>
+      <div className={css.infoIcon} />
+      <div>{text}</div>
+    </div>
+  );
+}
+
+export default MypageDataEmpty;
