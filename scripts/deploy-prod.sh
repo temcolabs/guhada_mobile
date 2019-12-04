d2c46af8c6fb41abb0ae08f73e5915cc583be2ad
Seoyoung Jeong <seoyoung.jeong@temco.io>
2019-12-04 15:37:38 +0900

    모바일, prod 배포시 서버2대에 동시 업로드

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,6 +1,7 @@
 # 배포 스크립트
 APP_NAME=guhada-mobile-prod
 HOST=mobile
+HOST2=mobile2
 USER=ec2-user
 REPOSITORY=git@github.com:temcolabs/guhada_mobile.git
 BRANCH=master
@@ -9,4 +10,5 @@ DEST_REPO=/home/guhada/web/guhada_mobile
 echo "> git pull && build app"
 cd $DEST_REPO && git reset --hard && git remote update --prune && git checkout $BRANCH && git pull && rm -rf .next && npm install && npm run build
 
-HOST=$HOST ./scripts/deploy-prod-upload.sh
+# HOST=$HOST ./scripts/deploy-prod-upload.sh
+HOST=$HOST1 ./scripts/deploy-prod-upload.sh & HOST=$HOST2 ./scripts/deploy-prod-upload.sh
