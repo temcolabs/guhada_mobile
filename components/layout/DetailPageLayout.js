78a961221fac38b475700a71ac0d682289db1769
Seoyoung Jeong <seoyoung.jeong@temco.io>
2019-12-06 18:57:30 +0900

    DetailPageLayout props 추가

--- components/layout/DetailPageLayout.js
+++ components/layout/DetailPageLayout.js
@@ -6,11 +6,22 @@ import css from './DetailPageLayout.module.scss';
  * 상세 페이지용 레이아웃
  * 제목이 있는 헤더와 뒤로가기 버튼이 있음
  */
-export default function DetailPageLayout({ pageTitle, children }) {
+export default function DetailPageLayout({
+  pageTitle,
+  children,
+  noPaddingBottom = false,
+}) {
   return (
     <div>
       <Header headerShape={'detailPage'} pageTitle={pageTitle} />
-      <div className={css.childrenWrap}>{children}</div>
+      <div
+        className={css.childrenWrap}
+        style={{
+          paddingBottom: noPaddingBottom ? 0 : undefined,
+        }}
+      >
+        {children}
+      </div>
     </div>
   );
 }
