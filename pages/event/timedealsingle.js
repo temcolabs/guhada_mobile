8b49487f676b3deb6c26fb6f7e6aaebd867ed066
kimkwan <kwan.kim@temco.io>
2020-04-10 14:27:45 +0900

    타임딜 싱글페이지 - 접속경로 체크 로직 수정

--- pages/event/timedealsingle.js
+++ pages/event/timedealsingle.js
@@ -1,15 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import TimeDealSingle from 'template/event/timedeal/TimeDealSingle';
 function TimedealSingle() {
+  const [dev, setDev] = useState(0);
+  const [local, setLocal] = useState(0);
   useEffect(() => {
-    if (process.env.NODE_ENV !== 'development') {
-      window.location.href = '/';
-    }
+    setDev(window.location.href.indexOf('dev'));
+    setLocal(window.location.href.indexOf('local'));
   }, []);
-  return (
-    <div>
-      {process.env.NODE_ENV === 'development' ? <TimeDealSingle /> : null}
-    </div>
-  );
+  if (dev === -1 && local === -1) {
+    window.location.href = '/';
+  }
+  return <div>{dev || local ? <TimeDealSingle /> : null}</div>;
 }
 export default TimedealSingle;
