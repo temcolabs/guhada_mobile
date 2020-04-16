1b2a7e4015732b78a0938ffd089c46b3b989a957
brianleenj <brianleenj@temco.io>
2020-04-16 12:26:51 +0900

    TECH-6505 : SEO 문구 change

--- childs/lib/constant/seo.js
+++ childs/lib/constant/seo.js
@@ -4,7 +4,7 @@ const sns = require('./sns');
 module.exports = {
   TITLE: '구하다',
   DESCRIPTION:
-    '14만+ 정품 명품 상품. 매주 이어지는 타임딜 특가. 220% 정품보장. 초저가 럭키드로우까지. 명품 정품 쇼핑의 기준 구하다',
+    '원하는 명품이 여기 다! 매주 이어지는 타임딜 특가. 220% 정품 보장. 초저가 럭키드로우까지. 명품 쇼핑의 정석 구하다',
 
   // window.location.origin에 해당하는 값
   BASE_URL: process.env.BASE_URL,
