9d9814fc4f76de4d400bcdd63786524ab7df8e79
brianleenj <brianleenj@temco.io>
2020-05-11 15:23:01 +0900

    deploy script changed.

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -4,9 +4,22 @@ APP_NAME=guhada-mobile-dev
 HOST=52.79.247.89
 USER=ec2-user
 REPOSITORY=git@github.com:temcolabs/guhada_mobile.git
-BRANCH=develop
+BRANCH=release
 DEST_REPO=/home/ec2-user/guhada_mobile
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
 
+RELEASE_VERSION=$(echo "release/${PACKAGE_VERSION}" | tr -d ' ')
+WINDOW_RELEASE_VERSION="release/v1.27.0"
+if [ $RELEASE_VERSION = "release/" ]
+then
+  RELEASE_VERSION=${WINDOW_RELEASE_VERSION}
+fi
+
+echo $RELEASE_VERSION
 # 서버 접속 후 테스트 앱 시작
 ssh -i ~/.ssh/guhada.pem $USER@$HOST "cd $DEST_REPO && git reset --hard && git remote update --prune && git checkout $BRANCH && git pull && npm install && npm run build && pm2 flush && pm2 reload ecosystem.config.js --only $APP_NAME --env production && ./scripts/delete-old-builds.sh && ./scripts/etc/createRobotsDisallowed.sh"
 
