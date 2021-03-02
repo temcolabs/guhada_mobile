9f1d744502170028188c253bff40e76521577589
Youngjun Kim <youngjun.kim@temco.io>
2021-03-02 18:59:46 +0900

    TECH-7386: feedId insertion on sign up (+ feedService.js added) default value is null

---
+++ childs/lib/API/user/feedService.js
@@ -0,0 +1,13 @@
+import API from 'childs/lib/API';
+
+export default {
+  /**
+   * check for available feeds
+   * ex) [{feedId: 1001, feedName: "NAVER"}]
+   */
+  matchFeedId: feedId => {
+    return API.user.get('/feeds').then(res => {
+      return res.data?.data.find(el => el.feedId === feedId);
+    });
+  },
+};
