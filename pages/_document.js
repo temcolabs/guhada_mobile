3c406943173c1c63ebb28efb4a4aadddcbeb0bb6
Youngjun Kim <youngjun.kim@temco.io>
2021-03-18 14:44:56 +0900

    TECH-8128: GA/GTM tag

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ class MyDocument extends Document {
           {/* Global site tag (gtag.js) - Google Analytics */}
           <script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=G-9SXJBX8EXX`}
+            src="https://www.googletagmanager.com/gtag/js?id=UA-145072876-1"
           />
           <script
             dangerouslySetInnerHTML={{
@@ -23,7 +23,16 @@ class MyDocument extends Document {
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
 
-              gtag('config', 'G-9SXJBX8EXX');`,
+              gtag('config', 'UA-145072876-1');`,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PN8SGDH');`,
             }}
           />
           {/* bootstrap 스타일시트(summernote의 dependency)*/}
