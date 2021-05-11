b856c2c226867d1cce13520d1e7967a3e4550bf5
chungil94 <chungil.lee@temco.io>
2021-05-11 11:52:47 +0900

    럭키드로우, 모달 오동작 이슈

--- components/mypage/DealOrdered.js
+++ components/mypage/DealOrdered.js
@@ -48,6 +48,11 @@ export default function DealOrdered({
 
   // 발송지연 on / off state
   const [isDeliveryDelayModalOpen, setDeliveryDelayModalOpen] = useState(false);
+  
+  const onCloseDelayModal = () => {
+    setDeliveryDelayModalOpen(false);
+    document.documentElement.style.overflow = 'initial';
+  };
 
   return (
     <>
@@ -55,7 +60,7 @@ export default function DealOrdered({
       {isDeliveryDelayModalOpen && (
         <DeliveryDelayModal
           isOpen={isDeliveryDelayModalOpen}
-          onClose={() => setDeliveryDelayModalOpen(false)}
+          onClose={() => onCloseDelayModal()}
           order={order}
         />
       )}
